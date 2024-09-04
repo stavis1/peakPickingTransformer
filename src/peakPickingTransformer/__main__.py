f7d65aa40ff7bc35ec930a515690b5fb4fc9c272
anon <stavis@vols.utk.edu>
2024-09-03 23:31:47 -0400

    layed out top level architecture

---
+++ src/peakPickingTransformer/__main__.py
@@ -0,0 +1,25 @@
+
+from peakPickingTransformer import argument_parser
+args = argument_parser()
+
+#parse mzML
+
+if args.task == 'train':
+    #initialize model
+    #run training
+    #report results    
+    pass
+
+elif args.task == 'refine':
+    #initialize model
+    #load weights
+    #run GUI
+    pass
+
+elif args.task == 'infer':
+    #initialize model
+    #load weights
+    #run inference
+    #report results
+    pass
+
