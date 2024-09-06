db9a64e8e6ff0a28a53c37f5f98bab1907f1a036
Steven Tavis <stavis@vols.utk.edu>
2024-09-06 17:51:32 -0400

    added MS1 peak class

---
+++ src/peakPickingTransformer/data_objects.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  6 17:05:23 2024
+
+@author: 4vt
+"""
+
+from dataclasses import dataclass
+import numpy as np
+
+#The dataclass decorator automatically adds several methods including __init__
+#The slots parameter makes attribute access slightly faster.
+@dataclass(slots = True)
+class ms1_peak():
+    '''This class holds the information about a single MS1 peak.'''
+    mz: float
+    rt: float
+    intensity: float
+    scan: int
+    psm: int = -1
+    vector: list = []
+
+    def preprocess(self):
+        '''
+        Encodes the MS1 peak data as a vector for use by the transformer model.
+        
+        arguments:
+            None
+        returns:
+            A list consisting of concatenated positional encodings for mz, rt, and intensity plus the psm number
+        side effects:
+            The .vector attribute gets defined if it was previously empty.   
+        
+        A -1 for the psm attribute means no psm is attached to this MS1 peak.
+        Any other value should be the (strictly positive) hash of the peptidoform
+        '''
+        if self.vector:
+            return self.vector
+        else:
+            pass #TO DO
+
+
+
+
+
