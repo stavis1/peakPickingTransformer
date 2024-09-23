2c94da57f078029a7196c5b107bf0abe4fa5f629
compbiolover <awillemscf@gmail.com>
2024-09-23 12:31:00 -0400

    Add transformer model class

---
+++ src/peakPickingTransformer/transformer_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  6 17:05:23 2024
+
+@author: compbiolover
+"""
+import sys
+
+class transformer():
+    '''This class holds the transformer model.'''
+    def transformer_model(self):
+        '''
+        This function defines the transformer model.
+        
+        arguments:
+            None
+        returns:
+            None
+        side effects:
+            None
+        '''
+        pass
