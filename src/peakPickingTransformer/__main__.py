c83d16644848da4371477a46c2b75c6f2ef0177a
anon <stavis@vols.utk.edu>
2024-10-15 18:12:42 -0400

    updated comments

--- src/peakPickingTransformer/__main__.py
+++ src/peakPickingTransformer/__main__.py
@@ -10,21 +10,31 @@ from peakPickingTransformer.transformer_model import transformer
 
 if args.task == 'train':
     from peakPickingTransformer.utilities import load_training_data, split_data, assess_model
-    model = transformer()
+    #load and process training data
     X, Y = load_training_data(args)
     X_train, Y_train, X_test, Y_test = split_data(X, Y, args)
+    
+    #fit model
+    model = transformer()
     model = model.fit(X_train, Y_train, args)
     model.dump(args)
+    
+    #make quality control plots
     assess_model(X_test, Y_test, model, args)   
 
 elif args.task == 'refine':
+    #initialize model
     model = transformer()
     model.load(args)
+
     #run GUI
 
 elif args.task == 'infer':
+    #initialize model
     model = transformer()
     model.load(args)
+
     #run inference
+
     #report results
 
